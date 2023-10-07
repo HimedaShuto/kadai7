97fde967ecb811f2a4d34fe0138bb37171ae2323
HimedaShuto <himedashuto123@gmail.com>
2023-10-08 01:43:36 +0900

    ブログ投稿一覧のページネーション

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,9 +9,10 @@ class PostController extends Controller
 {
      public function index(Post $post)
     {
-        return view('posts.index')->with(['posts'=>$post->get()]);
+        return view('posts.index')->with(['posts'=>$post->getPaginateByLimit()]);
         //blade内で使う変数'post'と設定。'posts'の中身ぬgetを使い、インスタンス化した$postを代入
     }
+    
 
 }
 ?>
