10c37c184c6ec39b60192c51dba5a82372348cc4
HimedaShuto <himedashuto123@gmail.com>
2023-10-10 00:18:13 +0900

    ブログ投稿ブログ投稿詳細の表示

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,6 +13,18 @@ class PostController extends Controller
         //blade内で使う変数'post'と設定。'posts'の中身ぬgetを使い、インスタンス化した$postを代入
     }
     
-
+    
+    /**
+     * 特定のIDのpostを表示する
+     * 
+     * @params 0bject Post //引数の$postはid=1のPostPostインスタンス
+     * @reurn Reposnse post view
+     */
+     
+     public function show(Post $post)
+     {
+         return view('posts.show')->with(['post'=>$post]);
+         //'post'はbladeファイルで使う変数.中身は$postはid=1のPostインスタンス
+     }
 }
 ?>
